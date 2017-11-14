5247ff99dd518451541c63a98288482c9919333d
YuLinChao <gash612@gmail.com>
2017-11-14 17:19:44 +0800

    [Fixed]修正運動系統 不管哪個方向都用狀態來做每秒位移 但是目前向上撞可以選要不要讓該塊障礙物(加第五參數)有碰撞效果 (雖然好像有BUG會穿過去我不知道為什麼還要再檢查) (對了還加了很多註解因為很煩躁)

--- src/main.js
+++ src/main.js
@@ -1,52 +1,62 @@
+//監聽:按下/放開 按鍵
 document.onkeydown = checkKey;
 document.onkeyup = checkKeyUp;
+//角色狀態控制
 var inTheAir = false;
 var droping = false;
+var onTheObstacle = false;
 var dropTime = 0;
-var timeInterval = 10;//0.01s
-var velocityY = 0;
-var velocityX = 0;
-var bottomY = 480;
-var leftKeyDown = false;
-var rightKeyDown = false;
-var jumpKeyDown = false;
 var jumpTimes = 0;
-var minYScroll = 300;
-var scrollMap = false;//捲動開關
-var obstacleSet = //[x1,x2,y1,y2]//碰到障礙物不要觸發重力
-{
-  "obstacle1" : [150,350,480,500],
-  "obstacle1_2" : [250,350,460,480],
-  "obstacle1_3" : [300,350,440,460],
-  "obstacle2" : [0,150,430,450],
-  "obstacle3" : [150,170,220,370],
-};
+var jumping = false;
+var jumpDistance = 50;
+var distanceCount = 0;
+var horizontalMoving = false;
+var horizontalDistance = 10;
+var horizontalCount = 0;
+//角色參數控制
 var ballBot;
 var ballCenter;
 var ballRadius = 10;
-var onTheObstacle = false;
+var timeInterval = 10;//0.01s
+var velocityY = 0;
+var velocityX = 0;
+var upV = 3;//2.5/0.01s
+//按鍵狀態控制
+var leftKeyDown = false;
+var rightKeyDown = false;
+var jumpKeyDown = false;
+//全域狀態控制
+var bottomY = 480;
+var minYScroll = 300;//Y卷軸相關速度
+var scrollMap = false;//捲動開關 開了就會捲動
+var obstacleSet = //[x1,x2,y1,y2,通行(option)]//碰到障礙物不要觸發重力
+{
+  "obstacle1" : [150,350,480,500],//stair
+  "obstacle1_2" : [250,350,460,480],//stair2
+  "obstacle1_3" : [300,350,440,460],//stair3
+  "obstacle2" : [0,150,430,450],//layer2
+  "obstacle3" : [150,170,220,370,false],//pink
+};
 
-function checkKeyUp(e) {
-  if(e.keyCode == '37' || e.keyCode =='39')
-  {
-    // left / right arrow up
+function checkKeyUp(e) {//放開按鍵 重製按鍵狀態 主要避免壓著不放連續觸發
+  if(e.keyCode == '37' || e.keyCode =='39')//左鍵右鍵放開的時候
+  {//判斷
     leftKeyDown = false;
     rightKeyDown = false;
   }
   if (e.keyCode == '38' || e.keyCode == '32') 
-  {
+  {//跳躍鍵放開
     //jumpKeyUp
     jumpKeyDown = false;
   }
 }
 
-function checkKey(e) {
+function checkKey(e) {//按下按鍵時觸發的
     
     e = e || window.event;
-
-    var orix = d3.select("#jumper").attr("cx");
+    var orix = d3.select("#jumper").attr("cx");//抓主角的X,Y
     var oriy = d3.select("#jumper").attr("cy");
-    //console.log(e.keyCode);
+    //console.log(e.keyCode);//用來看到底按了什麼
 
     if (e.keyCode == '38' || e.keyCode == '32') {
         // up arrow             
@@ -55,10 +65,11 @@ function checkKey(e) {
           //jumpKeyDown =true;
           if(jumpTimes<2)//跳兩段
           {
-            //jumpTimes +=1;
-            if(!leftKeyDown&&!rightKeyDown) velocityX = 0;
-            oriy = oriy-50;
-            d3.select("#jumper").attr("cy",oriy);
+            jumpTimes +=1;//算跳躍段數
+            //if(!leftKeyDown&&!rightKeyDown) velocityX = 0;
+            //oriy = oriy-50;
+            jumping = true;
+            //d3.select("#jumper").attr("cy",oriy);
           }          
         }        
     }
@@ -69,47 +80,19 @@ function checkKey(e) {
        // left arrow       
        leftKeyDown = true;
        velocityX = -1;
-       if(!inTheAir)
-        {
-         //velocityX = -0.5;
-         orix = orix-5;  
-         for (var i in obstacleSet)//撞到障礙物?
-         {
-            if((Number(oriy)-Number(ballRadius)>obstacleSet[i][2]&&Number(oriy)-Number(ballRadius)<obstacleSet[i][3])||(Number(oriy)+Number(ballRadius)<obstacleSet[i][3]&&Number(oriy)+Number(ballRadius)>obstacleSet[i][2]))
-            {
-              if(Number(orix)-Number(ballRadius)<obstacleSet[i][1]&&Number(orix)-Number(ballRadius)+5>=obstacleSet[i][1])//本來在他的右邊 進去後撞到右邊
-              {
-               orix = obstacleSet[i][1]+ballRadius;
-               break;
-              }
-            }
-         }     
-         d3.select("#jumper").attr("cx",orix);       
-       }       
+       //if(!inTheAir)
+       // {
+          horizontalMoving = true;    
+       //}       
     }
     else if (e.keyCode == '39') {
        // right arrow
        rightKeyDown = true;
        velocityX = 1;
-       if(!inTheAir)
-       {
-         //velocityX = 0.5;
-         orix = Number(orix)+5;
-         console.log("oriy",oriy);
-         for (var i in obstacleSet)//撞到障礙物?
-         {
-            if((Number(oriy)-Number(ballRadius)>obstacleSet[i][2]&&Number(oriy)-Number(ballRadius)<obstacleSet[i][3])||(Number(oriy)+Number(ballRadius)<obstacleSet[i][3]&&Number(oriy)+Number(ballRadius)>obstacleSet[i][2]))
-            {
-              console.log(obstacleSet[i]);
-              if(Number(orix)+Number(ballRadius)>obstacleSet[i][0]&&Number(orix)+Number(ballRadius)-5<=obstacleSet[i][0])
-              {
-               orix = obstacleSet[i][0]-ballRadius;
-               break;
-              }
-            }
-         }   
-         d3.select("#jumper").attr("cx",orix);
-       }
+       //if(!inTheAir)
+       //{
+        horizontalMoving = true;
+       //}
     }
 }
 
@@ -128,7 +111,7 @@ function worldGravity()
   var hittedObstalceY;//撞到的障礙物的Y
   var distanceX;
   var distanceY;
-  //v=gt 初速度=0
+  //v=gt 初速度=0  
   if(droping)
   {
     dropTime = (Number(dropTime)+Number(timeInterval));
@@ -158,7 +141,67 @@ function worldGravity()
       onTheObstacle =false;
     }
   }
-  if(onTheObstacle)
+  if(horizontalMoving)//橫向移動中
+  {
+
+    var orix = d3.select("#jumper").attr("cx");
+    var oriy = d3.select("#jumper").attr("cy");
+    orix = Number(orix)+Number(velocityX);
+    horizontalCount = Number(horizontalCount)+Math.abs(velocityX);
+    for (var i in obstacleSet)//撞到障礙物?
+    {//上緣在某block的寬度間 或 下緣在某block的寬度間 =>表示同一高度
+        if((Number(oriy)-Number(ballRadius)>obstacleSet[i][2]&&Number(oriy)-Number(ballRadius)<obstacleSet[i][3])||
+          (Number(oriy)+Number(ballRadius)<obstacleSet[i][3]&&Number(oriy)+Number(ballRadius)>obstacleSet[i][2])||
+          (Number(oriy)<obstacleSet[i][3]&&Number(oriy)>obstacleSet[i][2]))
+        {//球左緣本來在他的右邊 進去後撞到右邊 或 球右緣本來在他左邊 進去後撞到右邊
+          if((Number(orix)-Number(ballRadius)<=obstacleSet[i][1]&&Number(orix)-Number(ballRadius)+Number(velocityX)>=obstacleSet[i][1])||
+            (Number(orix)+Number(ballRadius)>=obstacleSet[i][0]&&Number(orix)+Number(ballRadius)-velocityX<=obstacleSet[i][0]))
+          {
+           orix = orix - velocityX;
+           horizontalMoving = false;//撞到要停下移動狀態
+           horizontalCount = 0;
+           break;
+          }
+        }
+    } 
+    d3.select("#jumper").attr("cx",orix);
+    if(horizontalCount>=horizontalDistance)
+    {
+      horizontalMoving = false;
+      horizontalCount = 0 ;
+    }
+  }
+
+  if(jumping)
+  {
+    var oriy = d3.select("#jumper").attr("cy");
+    oriy = Number(oriy)-Number(upV);//向上用扣的
+    distanceCount = Number(distanceCount)+Math.abs(upV);
+    for (var i in obstacleSet)//撞到障礙物?
+    {//中心在某block的左右寬度間 或 中心在某block的左右寬度間 =>表示同一X範圍
+      if(obstacleSet[i].length==5)
+        if((Number(orix)-Number(ballRadius)>obstacleSet[i][0]&&Number(orix)-Number(ballRadius)<obstacleSet[i][1])||
+          (Number(orix)+Number(ballRadius)>obstacleSet[i][0]&&Number(orix)+Number(ballRadius)<obstacleSet[i][1])||
+          (Number(orix)>obstacleSet[i][0]&&Number(orix)<obstacleSet[i][1]))
+        {//球上緣本來在他的下邊 進去後撞到下邊
+          if(Number(oriy)-Number(ballRadius)>=obstacleSet[i][3]&&Number(oriy)-Number(ballRadius)-Number(upV)<=obstacleSet[i][3])
+          {
+           oriy = obstacleSet[i][3]+ballRadius;
+           jumping = false;//撞到要停下移動狀態
+           distanceCount = 0;
+           break;
+          }
+        }
+    }
+    d3.select("#jumper").attr("cy",oriy);
+    if(distanceCount>=jumpDistance)
+    {
+      jumping=false;
+      distanceCount=0;
+      droping=true;
+    }
+  }
+  else if(onTheObstacle)
   {
     jumpTimes = 0;
     inTheAir = false;
@@ -181,8 +224,8 @@ function worldGravity()
       oriy=bottomY;
     }
 
-    distanceX = velocityX;
-    orix = Number(orix) + Number(distanceX);
+    //distanceX = velocityX;
+    //orix = Number(orix) + Number(distanceX);
     d3.select("#jumper").attr("cy",oriy);
     d3.select("#jumper").attr("cx",orix);
   }
