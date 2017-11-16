69c788249ae8e242645b16d8d0634173a140100a
YuLinChao <gash612@gmail.com>
2017-11-16 18:32:56 +0800

    [fix]修正了亂寫的左右碰撞 寫左右碰撞的時候腦子不清楚，現在修正成能正常運作的版本 順便刪除一些多餘的程式
    [todo]
    向下墜落的時候有可能半身進障礙物(碰撞敘述句的關係)(已知原因，待修)
    向上的時候有時候會穿過不該穿過的物體(猜測原因跟向下判斷式類似)(未知原因，先修向下)

--- src/main.js
+++ src/main.js
@@ -18,9 +18,9 @@ var ballBot;
 var ballCenter;
 var ballRadius = 10;
 var timeInterval = 10;//0.01s
-var velocityY = 0;
+var velocityY = 0;//拿去算自由落體了
 var velocityX = 0;
-var upV = 3;//2.5/0.01s
+var upV = -3;//2.5/0.01s 這個其實就是真正的Vy 負號向上
 //按鍵狀態控制
 var jumpKeyDown = false;
 var leftKeyDown = false;//用於判斷左右鍵押著不放的情況
@@ -31,6 +31,7 @@ var minYScroll = 300;//Y卷軸相關速度
 var scrollMap = false;//捲動開關 開了就會捲動
 var obstacleSet = //[x1,x2,y1,y2,通行(option)]//碰到障礙物不要觸發重力
 {
+  "floor":[0,500,500,520],
   "obstacle1" : [150,350,480,500],//stair
   "obstacle1_2" : [250,350,460,480],//stair2
   "obstacle1_3" : [300,350,440,460],//stair3
@@ -115,7 +116,6 @@ function worldGravity()
     d3.select("#basicSVG").attr("viewBox","0,"+minYScroll+",500,500")
   }
   var gravity = 0.3;
-  var hittedObstalceY;//撞到的障礙物的Y
   var distanceX;
   var distanceY;
   //v=gt 初速度=0  
@@ -125,27 +125,17 @@ function worldGravity()
   }  
   velocityY = gravity * dropTime/1000;
   distanceY = velocityY*timeInterval;
-  var oriy = d3.select("#jumper").attr("cy");
-  var orix = d3.select("#jumper").attr("cx");
-
+  
   for (var i in obstacleSet)//踩在障礙物上嗎
   {
+    onTheObstacle=false;
     if(ballCenter>=obstacleSet[i][0]&&ballCenter<=obstacleSet[i][1])
     {
-      if((ballBot-obstacleSet[i][2])<5&&(ballBot-obstacleSet[i][2])>=0)
+      if(Math.abs(ballBot-obstacleSet[i][2])<=1)
       {
        onTheObstacle=true;
-       hittedObstalceY=obstacleSet[i][2];
        break;
       }
-      else
-      {
-        onTheObstacle =false;
-      }
-    }
-    else
-    {
-      onTheObstacle =false;
     }
   }
   if(horizontalMoving)//橫向移動中
@@ -157,12 +147,12 @@ function worldGravity()
     horizontalCount = Number(horizontalCount)+Math.abs(velocityX);
     for (var i in obstacleSet)//撞到障礙物?
     {//上緣在某block的寬度間 或 下緣在某block的寬度間 =>表示同一高度
-        if((Number(oriy)-Number(ballRadius)>obstacleSet[i][2]&&Number(oriy)-Number(ballRadius)<obstacleSet[i][3])||
+        if((Number(oriy)-Number(ballRadius)<obstacleSet[i][3]&&Number(oriy)-Number(ballRadius)>obstacleSet[i][2])||
           (Number(oriy)+Number(ballRadius)<obstacleSet[i][3]&&Number(oriy)+Number(ballRadius)>obstacleSet[i][2])||
           (Number(oriy)<obstacleSet[i][3]&&Number(oriy)>obstacleSet[i][2]))
         {//球左緣本來在他的右邊 進去後撞到右邊 或 球右緣本來在他左邊 進去後撞到右邊
-          if((Number(orix)-Number(ballRadius)<=obstacleSet[i][1]&&Number(orix)-Number(ballRadius)+Number(velocityX)>=obstacleSet[i][1])||
-            (Number(orix)+Number(ballRadius)>=obstacleSet[i][0]&&Number(orix)+Number(ballRadius)-velocityX<=obstacleSet[i][0]))
+          if((Number(orix)-Number(ballRadius)>=obstacleSet[i][1]&&Number(orix)-Number(ballRadius)+Number(velocityX)<=obstacleSet[i][1])||
+            (Number(orix)+Number(ballRadius)<=obstacleSet[i][0]&&Number(orix)+Number(ballRadius)+Number(velocityX)>=obstacleSet[i][0]))
           {
            orix = orix - velocityX;
            horizontalMoving = false;//撞到要停下移動狀態
@@ -179,10 +169,11 @@ function worldGravity()
     }
   }
 
-  if(jumping)
+  if(jumping)//跳躍中
   {
+    var orix = d3.select("#jumper").attr("cx");
     var oriy = d3.select("#jumper").attr("cy");
-    oriy = Number(oriy)-Number(upV);//向上用扣的
+    oriy = Number(oriy)+Number(upV);
     distanceCount = Number(distanceCount)+Math.abs(upV);
     for (var i in obstacleSet)//撞到障礙物?
     {//中心在某block的左右寬度間 或 中心在某block的左右寬度間 =>表示同一X範圍
@@ -191,7 +182,7 @@ function worldGravity()
           (Number(orix)+Number(ballRadius)>obstacleSet[i][0]&&Number(orix)+Number(ballRadius)<obstacleSet[i][1])||
           (Number(orix)>obstacleSet[i][0]&&Number(orix)<obstacleSet[i][1]))
         {//球上緣本來在他的下邊 進去後撞到下邊
-          if(Number(oriy)-Number(ballRadius)>=obstacleSet[i][3]&&Number(oriy)-Number(ballRadius)-Number(upV)<=obstacleSet[i][3])
+          if(Number(oriy)-Number(ballRadius)>=obstacleSet[i][3]&&Number(oriy)-Number(ballRadius)+Number(upV)<=obstacleSet[i][3])
           {
            oriy = obstacleSet[i][3]+ballRadius;
            jumping = false;//撞到要停下移動狀態
@@ -208,20 +199,34 @@ function worldGravity()
       droping=true;
     }
   }
-  else if(onTheObstacle)
+  else if(onTheObstacle)//障礙物上
   {
     jumpTimes = 0;
     inTheAir = false;
     droping = false;
     dropTime = 0;
-    oriy=hittedObstalceY-10;//依據球的半徑算碰撞
-    d3.select("#jumper").attr("cy",oriy);
-  }
-  else if(oriy<bottomY)//在空中
+  }  
+  else //自由落體
   {
+    var oriy = d3.select("#jumper").attr("cy");
+    var orix = d3.select("#jumper").attr("cx");
     inTheAir = true;
     droping = true;
     oriy = Number(oriy) + distanceY;
+    
+    for (var i in obstacleSet)//踩在障礙物上嗎
+    {
+      if(ballCenter>=obstacleSet[i][0]&&ballCenter<=obstacleSet[i][1])
+      {
+        //加了距離後跑進障礙物中
+        if((Number(oriy)+Number(ballRadius)+Number(distanceY)>=obstacleSet[i][2])&&
+        (Number(oriy)+Number(ballRadius)+Number(distanceY)<=obstacleSet[i][3]))
+        {
+         oriy = obstacleSet[i][2]-ballRadius;//中心歸位到障礙物上緣-球半徑
+         break;
+        }
+      }
+    }
     if(oriy>bottomY)//撞到地板 重製跳躍狀態
     {
       jumpTimes = 0;
@@ -230,14 +235,8 @@ function worldGravity()
       dropTime = 0;
       oriy=bottomY;
     }
-
-    //distanceX = velocityX;
-    //orix = Number(orix) + Number(distanceX);
     d3.select("#jumper").attr("cy",oriy);
-    d3.select("#jumper").attr("cx",orix);
   }
-  //console.log(onTheObstacle);
-  
 }
 
 function init()
